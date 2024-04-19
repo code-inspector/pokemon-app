29f10ad47e95f0aa8c260f254f83ef46c34369cd
code-inspector <azanb30@gmail.com>
2024-04-19 17:32:03 -0400

    Create and add pokemon card and modal component in home

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
-export const apiBaseUrl = 'https://pokeapi.co/api/v2';
+export const apiBaseUrl: string = 'https://pokeapi.co/api/v2';
+export const mediaApiBaseUrl: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PokemonCard from '../../components/pokemonCard/PokemonCard';
+import { Col, Container, Row } from 'react-bootstrap';
+import PokemonModal from '../../components/pokemonModal/PokemonModal';
 
-export default function home() {
-  return <div>home</div>;
+interface Pokemon {
+  name: string;
+  url: string;
 }
+
+const pokemonData: Pokemon[] = [
+  {
+    name: 'bulbasaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  },
+  {
+    name: 'ivysaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/2/',
+  },
+  {
+    name: 'venusaur',
+    url: 'https://pokeapi.co/api/v2/pokemon/3/',
+  },
+  {
+    name: 'charmander',
+    url: 'https://pokeapi.co/api/v2/pokemon/4/',
+  },
+  {
+    name: 'charmeleon',
+    url: 'https://pokeapi.co/api/v2/pokemon/5/',
+  },
+  {
+    name: 'charizard',
+    url: 'https://pokeapi.co/api/v2/pokemon/6/',
+  },
+  {
+    name: 'squirtle',
+    url: 'https://pokeapi.co/api/v2/pokemon/7/',
+  },
+  {
+    name: 'wartortle',
+    url: 'https://pokeapi.co/api/v2/pokemon/8/',
+  },
+  {
+    name: 'blastoise',
+    url: 'https://pokeapi.co/api/v2/pokemon/9/',
+  },
+];
+
+export const Home = () => {
+  const [modal, setModal] = useState<boolean>(false);
+  const [selectedPokemonUrl, setSelectedPokemonUrl] = useState<string>('');
+
+  const onCardClick = (url: string) => {
+    console.log('clicked');
+    setModal(true);
+    setSelectedPokemonUrl(url);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  return (
+    <Container>
+      <h1>Pokemon List</h1>
+      <Row>
+        {pokemonData.map((pokemon, index) => (
+          <Col key={index} className="mb-5">
+            <PokemonCard pokemon={pokemon} index={index + 1} onCardClick={onCardClick} />
+          </Col>
+        ))}
+      </Row>
+      <PokemonModal show={modal} onHide={closeModal} selectedPokemonUrl={selectedPokemonUrl}/>
+    </Container>
+  );
+};
+
+export default Home;
