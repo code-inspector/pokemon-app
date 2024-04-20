039eef45e782304f6a575fd8e7e79a5ac9c95e9a
code-inspector <azanb30@gmail.com>
2024-04-20 13:12:33 -0400

    Update modal loading state

--- src/components/pokemonModal/PokemonModal.tsx
+++ src/components/pokemonModal/PokemonModal.tsx
@@ -12,53 +12,56 @@ interface PokemonModalProps {
 const PokemonModal = ({ onHide, id }: PokemonModalProps) => {
   const {
     data: pokemon,
-    isLoading,
+    isFetching,
     isError,
   } = useFetchPokemonByIdQuery(id, {
     skip: !id,
   });
 
-  if (isLoading)
-    return (
-      <Container className={`d-flex justify-content-center align-items-center ${style.loading}`}>
-        <Spinner animation="border" />
-      </Container>
-    );
   if (isError) return <Container>Error</Container>;
 
   return (
-    <Modal
-      show={!!id && !!pokemon && !isLoading}
-      onHide={onHide}
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
+    <Modal show={!!id} onHide={onHide} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Title>{pokemon?.name}</Modal.Title>
       </Modal.Header>
-      <div className="d-flex justify-content-center align-items-center">
-        <img src={pokemon?.sprites?.front_default} alt="Pokemon" className="w-50" />
-      </div>
+      {isFetching && (
+        <Container className={`d-flex justify-content-center align-items-center ${style.loading}`}>
+          <Spinner animation="border" />
+        </Container>
+      )}
+      {pokemon && !isFetching && (
+        <>
+          <div className="d-flex justify-content-center align-items-center">
+            <img src={pokemon?.sprites?.front_default} alt="Pokemon" className="w-50" />
+          </div>
 
-      <span className="m-4 d-flex justify-content-between">
-        <span className={style.title}>Name:</span>
-        <span>{pokemon?.name}</span>
-      </span>
-      <hr />
-      <span className="m-4 d-flex justify-content-between">
-        <span className={style.title}>Height:</span>
-        <span>{pokemon?.height}</span>
-      </span>
-      <hr />
-      <span className="m-4 d-flex justify-content-between">
-        <span className={style.title}>Weight:</span>
-        <span>{pokemon?.weight}</span>
-      </span>
-      <hr />
-      <span className="m-4 d-flex justify-content-between">
-        <span className={style.title}>Types:</span>
-        <span>{pokemon?.types?.join(', ')}</span>
-      </span>
+          <span className="m-4 d-flex justify-content-between">
+            <span className={style.title}>Name:</span>
+            <span>{pokemon?.name}</span>
+          </span>
+          <hr />
+          <span className="m-4 d-flex justify-content-between">
+            <span className={style.title}>Height:</span>
+            <span>{pokemon?.height}</span>
+          </span>
+          <hr />
+          <span className="m-4 d-flex justify-content-between">
+            <span className={style.title}>Weight:</span>
+            <span>{pokemon?.weight}</span>
+          </span>
+          <hr />
+          <span className="m-4 d-flex justify-content-between">
+            <span className={style.title}>Types:</span>
+            <span>{pokemon?.types?.join(', ')}</span>
+          </span>
+        </>
+      )}
+      {isError && !isFetching && (
+        <Container className={`d-flex justify-content-center align-items-center ${style.loading}`}>
+          Unable to fetch details
+        </Container>
+      )}
     </Modal>
   );
 };
